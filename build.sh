46c1a4dfddb7c2b86a701952ee83af29f602e81e
tingqiu814 <skin20120726@sina.cn>
2019-08-18 12:00:26 +0800

    add build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,16 @@
-gcc -o ./bin/uts uts.c
+#!/bin/bash 
+
+cd `dirname $0`;
+BASEDIR=$(pwd)
+# PRONAME=`basename $BASEDIR`
+if [ $# != 1 ];then
+		echo $0 proname
+		exit
+fi
+PRONAME=$1
+
+if ! [ -d bin ] ; then
+	mkdir bin
+fi
+
+gcc -o ./bin/$PRONAME "$PRONAME.c"
